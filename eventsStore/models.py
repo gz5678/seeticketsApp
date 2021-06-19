5ee9f00cff1453f0b1259187761491de50f6aaed
gz5678 <gil.zivor@mail.huji.ac.il>
2021-06-20 01:47:47 +0300

    Change to declarative syntax and added product table

--- eventsStore/models.py
+++ eventsStore/models.py
@@ -1,10 +1,26 @@
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import mapper
 
 
-events = Table('events', metadata,
-               Column('id', Integer, primary_key=True, autoincrement=True),
-               Column('name', String(50), unique=True),
-               Column('serviceFeeAmount', Integer),
-               Column('serviceFeeCurrency', String))
+class Events(Base):
+    __tablename__ = 'events'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    name = Column('name', String(50), unique=True)
+    serveFeeAmount = Column('serviceFeeAmount', Integer)
+    serviceFeeCurrency = Column('serviceFeeCurrency', String)
 
+    def __init__(self, name=None, serveFeeAmount=None, serviceFeeCurrency=None):
+        self.name = name
+        self.serveFeeAmount = serveFeeAmount
+        self.serviceFeeCurrency = serviceFeeCurrency
+
+
+class Products(Base):
+    __tablename__ = 'products'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    name = Column('name', String(50))
+    eventId = Column('eventId', Integer, ForeignKey("events.id"))
+
+    def __init__(self, name=None, eventId=None):
+        self.name = name
+        self.eventId = eventId
