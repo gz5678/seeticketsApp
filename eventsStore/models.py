fdcd7db3088994a98ad2073488afe53d2c79bc8c
gz5678 <gil.zivor@mail.huji.ac.il>
2021-06-20 02:04:13 +0300

    Added products to events mapping table

--- eventsStore/models.py
+++ eventsStore/models.py
@@ -6,21 +6,33 @@ class Events(Base):
     __tablename__ = 'events'
     id = Column('id', Integer, primary_key=True, autoincrement=True)
     name = Column('name', String(50), unique=True)
-    serveFeeAmount = Column('serviceFeeAmount', Integer)
-    serviceFeeCurrency = Column('serviceFeeCurrency', String)
+    service_fee_amount = Column('service_fee_amount', Integer)
+    service_fee_currency = Column('service_fee_currency', String)
 
-    def __init__(self, name=None, serveFeeAmount=None, serviceFeeCurrency=None):
+    def __init__(self, name=None, service_fee_amount=None, service_fee_currency=None):
         self.name = name
-        self.serveFeeAmount = serveFeeAmount
-        self.serviceFeeCurrency = serviceFeeCurrency
+        self.service_fee_amount = service_fee_amount
+        self.service_fee_currency = service_fee_currency
 
 
 class Products(Base):
     __tablename__ = 'products'
     id = Column('id', Integer, primary_key=True, autoincrement=True)
     name = Column('name', String(50))
-    eventId = Column('eventId', Integer, ForeignKey("events.id"))
+    service_fee_amount = Column('service_fee_amount', Integer)
+    service_fee_currency = Column('service_fee_currency', String)
 
-    def __init__(self, name=None, eventId=None):
+    def __init__(self, name=None, service_fee_amount=None, service_fee_currency=None):
         self.name = name
-        self.eventId = eventId
+        self.service_fee_amount = service_fee_amount
+        self.service_fee_currency = service_fee_currency
+
+
+class ProductsToEvents(Base):
+    __tablename__ = 'productsToEvents'
+    event_id = Column('event_id', Integer, ForeignKey(Events.id))
+    product_id = Column('product_id', Integer, ForeignKey(Products.id))
+
+    def __init__(self, event_id=None, product_id=None):
+        self.event_id = event_id
+        self.product_id = product_id
