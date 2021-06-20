317e22a766558e18a96254c4b553bb6152f10585
gz5678 <gil.zivor@mail.huji.ac.il>
2021-06-20 04:01:49 +0300

    Refactor into factory structure with config file

--- eventsStore/__init__.py
+++ eventsStore/__init__.py
@@ -1,10 +1,17 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile("../config.py")
 
-from eventsStore.models import Products, Events, association_table
+    db.init_app(app)
+    from eventsStore.models import Products, Events, association_table
+
+    with app.app_context():
+        db.create_all()
+
+    return app
