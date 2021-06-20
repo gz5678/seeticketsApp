230e5efe64662885d0c205e876028663721cf348
gz5678 <gil.zivor@mail.huji.ac.il>
2021-06-20 19:58:52 +0300

    Redirect from chooseEvent to chooseProducts

--- eventsStore/feeCalculator.py
+++ eventsStore/feeCalculator.py
@@ -1,12 +1,19 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, request, redirect, url_for
 )
-from eventsStore.models import Events
+from eventsStore.models import Events, Products
 
 bp = Blueprint('calculators', __name__)
 
-@bp.route('/')
+
+@bp.route('/', methods=('POST',))
 def chooseEvent():
+    if request.method == 'POST':
+        eventName = request.form['event']
+        eventId = Events.query.filter_by(name=eventName).first().id
+        products = Products.query.all()
+        productsNames = [product.name for product in products]
+        redirect(url_for('calculators.chooseProducts'))
     events = Events.query.all()
     eventNames = [event.name for event in events]
     return render_template('chooseEvent.html', events=eventNames)
