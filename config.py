8a8411df79f38fb4e26b10dae1b2e06d15335c39
gz5678 <gil.zivor@mail.huji.ac.il>
2021-06-21 03:12:10 +0300

    If uknown event is passed to choose product or no event is passed at all, return to choose event

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+SECRET_KEY = 'dev'
--- eventsStore/feeCalculator.py
+++ eventsStore/feeCalculator.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, request, redirect, url_for
+    Blueprint, render_template, request, redirect, url_for, flash
 )
 from eventsStore import db
 from eventsStore.models import Events, Products, association_table
@@ -16,11 +16,16 @@ def chooseEvent():
 
 @bp.route('/chooseProducts')
 def chooseProducts():
-    #TODO: IF NO EVENT WAS GIVEN (PAGE WAS ACCESSED DIRECTLY) OR THE EVENT DOESN'T EXIST IN DATABSE, NEED TO RETURN TO CHOOSEEVENT PAGE
+    error = None
     eventName = request.args.get('event')
-    eventId = Events.query.filter_by(name=eventName).first().id
-    products = Products.query.join(association_table,
-                                   association_table.c.product_id == Products.id).\
-        filter(association_table.c.event_id == eventId)
-    productsNames = [product.name for product in products]
-    return render_template('chooseProducts.html', products=productsNames)
+    event = Events.query.filter_by(name=eventName).first()
+    if not event:
+        error = "No event was given or event doesn't exist"
+        flash(error)
+        return redirect(url_for('calculators.chooseEvent'))
+    else:
+        products = Products.query.join(association_table,
+                                       association_table.c.product_id == Products.id).\
+            filter(association_table.c.event_id == event.id)
+        productsNames = [product.name for product in products]
+        return render_template('chooseProducts.html', products=productsNames)
