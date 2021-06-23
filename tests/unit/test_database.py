b956b07476e57dc21deb8c6c24d0105eed0b3734
gz5678 <gil.zivor@mail.huji.ac.il>
2021-06-23 11:56:01 +0300

    Do deletion test on 2 products instead of 1

--- tests/unit/test_database.py
+++ tests/unit/test_database.py
@@ -4,7 +4,8 @@ from eventsStore.models import Events, Products
 def test_deleting_event_deletes_the_related_products(test_db):
     event1 = Events(name="Tomorrowland", service_fee_amount=5, service_fee_currency="USD")
     product1 = Products(name="VIP", service_fee_amount=15, service_fee_currency="USD")
-    event1.products.append(product1)
+    product2 = Products(name="Weekend", service_fee_amount=10, service_fee_currency="USD")
+    event1.products.extend([product1, product2])
     test_db.session.add(event1)
     test_db.session.commit()
 
