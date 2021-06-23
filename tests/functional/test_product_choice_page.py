c6927eaa22a52e913aaa8a1c4770d0a120d8d1a3
gz5678 <gil.zivor@mail.huji.ac.il>
2021-06-23 12:18:40 +0300

    Test all products appear

--- tests/functional/test_product_choice_page.py
+++ tests/functional/test_product_choice_page.py
@@ -1,3 +1,10 @@
+from eventsStore.models import Events
+
 
 def test_all_products_appear(test_client, init_database):
-    pass
+    events = Events.query.all()
+    for event in events:
+        response = test_client.get('/chooseProducts?event={}'.format(event.name), follow_redirects=True)
+        for product in event.products:
+            assert bytes(product.name, encoding='utf-8') in response
+
